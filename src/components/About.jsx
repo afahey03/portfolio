146ed0fddec34b0ba4447cb94fd22db41174abed
Aidan Fahey <afahey2003@yahoo.com>
2025-11-17 16:47:09 -0500

    better education section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const About = () => {
+    const education = [
+        {
+            degree: 'B.A. Computer Science & Religious Studies',
+            institution: 'College of the Holy Cross',
+            years: '2021 - 2025'
+        }
+    ];
+
     return (
         <section id="about">
             <h2 className="section-title">About Me</h2>
@@ -10,10 +18,16 @@ const About = () => {
                     I thrive on solving complex problems and creating innovative solutions that make a real impact in
                     the digital world.
                 </p>
-                <p className="education-text">
-                    B.A. Computer Science & Religious Studies<br />
-                    College of the Holy Cross 2025
-                </p>
+                <div className="education-section">
+                    <h3 className="education-heading">Education</h3>
+                    {education.map((edu, index) => (
+                        <div key={index} className="education-item">
+                            <div className="education-degree">{edu.degree}</div>
+                            <div className="education-institution">{edu.institution}</div>
+                            <div className="education-years">{edu.years}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
