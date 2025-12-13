05e6331a214e2c933a092f724fbb9e82a7baaaf1
Aidan Fahey <afahey2003@yahoo.com>
2025-12-13 09:44:08 -0500

    fix experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
             company: 'The Kenerson Group',
             companyUrl: 'https://kenersongroup.com',
             date: 'December 2025 - Present • Athol, MA',
-            description: 'Geospatial and full-stack development of TreeWorks, an urban tree management system built as an extension to Esri\'s ArcGIS Pro® and ArcGIS Online platforms.',
+            description: 'Software development in C# / .NET of TreeWorks®, an urban tree management system built as an extension to Esri\'s ArcGIS Pro® and ArcGIS Online platforms.',
             logo: 'TKG.webp'
         },
         {
